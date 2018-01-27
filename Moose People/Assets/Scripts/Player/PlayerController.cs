140f84ec71c34d3872a1cbc616a9ba36559befb5
Stefan Scorse <stefan.scorse.92@gmail.com>
2018-01-27 21:48:40 +1100

    update 1
    camera works wasd with mouse
    
    movement right click kinda works

---
+++ Moose People/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour 
+{
+	private NavMeshAgent agent;
+
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(1))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit = hit;
+			Physics.Raycast(ray, out hit);
+			if (hit.transform)
+			{
+				agent.SetDestination(hit,point);
+			}
+		}
+	}
+}
